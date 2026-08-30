2572bed5b4d8730372ec11c5bb094c07f09d01bd
st192331 <st192331@stud.uni-stuttgart.de>
2026-08-30 17:17:15 +0200

    Experiment with hyper parameter - current spearman: 0.304

--- src/train.py
+++ src/train.py
@@ -23,7 +23,8 @@ train_dataset = Dataset.from_dict({
 # Training Arguments
 args = SentenceTransformerTrainingArguments(
     output_dir=output_path1,
-    num_train_epochs=30,
+    learning_rate=2e-5,
+    num_train_epochs=50,
     per_device_train_batch_size=16,
     warmup_steps=100,
 )
